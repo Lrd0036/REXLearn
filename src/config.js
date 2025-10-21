0647c271f950ad165340e0259e02ab01e4afa61d
Lrd0036 <lrd0036@auburn.edu>
2025-10-20 19:52:05 -0500

    API fixed

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   SUPABASE_URL: "https://qigdygawsjiwrotzuwpd.supabase.co",
-  SUPABASE_ANON_KEY: "11b2fee32bb7a16dcd8df183cdc96fe50bf...<add rest of key from secrets>",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpZ2R5Z2F3c2ppd3JvdHp1d3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NzczNjMsImV4cCI6MjA3NjU1MzM2M30.dnI-VGZhVReR7E9FdxRwFwH_ay8pAYgXrstMNyX9d-A"
 
   // Edge function endpoints
   EDGE_FUNCTIONS: {
